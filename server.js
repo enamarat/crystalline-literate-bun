09272d8b102e19b97772c94240faad889ece1c3e
Glitch (crystalline-literate-bun) <none>
2020-06-02 11:26:59 +0000

    🐪🎥 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var express = require('express');
+var cors = require('cors');
+// require and use "multer"...
+var multer = require("multer");
+var upload = multer({ dest: 'uploads/' });
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+/*** My solution ***/
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+})
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
